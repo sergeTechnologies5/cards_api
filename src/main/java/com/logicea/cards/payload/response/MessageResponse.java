5cf102529aa8d923cbdf624aa8e401017e86f9aa
Phineas Njeru <njesh74@gmail.com>
2023-06-10 00:56:26 +0300

    Inital Project

---
+++ src/main/java/com/logicea/cards/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.logicea.cards.payload.response;
+
+public class MessageResponse {
+  private String message;
+
+  public MessageResponse(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
---
+++ src/main/java/com/logicea/cards/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.logicea.cards.repository;
+
+import com.logicea.cards.models.ERole;
+import com.logicea.cards.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
